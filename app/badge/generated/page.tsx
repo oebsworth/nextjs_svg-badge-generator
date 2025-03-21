b35efe5ad086bdec14d40cc7a1b55baeb1a344c5
Oliver Ebsworth <work.ebswortholiver@gmail.com>
2025-03-21 17:19:24 +0000

    feat: Added functionality to receive the `searchParams`.

--- app/badge/generated/page.tsx
+++ app/badge/generated/page.tsx
@@ -1,2 +1,23 @@
+"use client"
+import { useSearchParams } from 'next/navigation'
+import { useState, useEffect } from "react"
 export default function GeneratedPage() {
+    const searchParams = useSearchParams()
+
+    const [params, setParams] = useState<Record<string, string>>({
+        text: '',
+        svgName: '',
+        backgroundColor: '000000',
+        textColor: 'ffffff',
+        iconColor: 'ffffff'
+    })
+    useEffect(() => {
+        const text = searchParams.get('text') || ''
+        const svgName = searchParams.get('svgName') || ''
+        const backgroundColor = searchParams.get('backgroundColor') || '000000'
+        const textColor = searchParams.get('textColor') || 'ffffff'
+        const iconColor = searchParams.get('iconColor') || 'ffffff'
+
+        setParams({ text, svgName, backgroundColor, textColor, iconColor })
+    }, [searchParams])
 }
