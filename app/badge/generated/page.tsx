c37a3956e5f02ef33888dffc40cd7a25ff6f9939
Oliver Ebsworth <work.ebswortholiver@gmail.com>
2025-03-21 17:22:33 +0000

    feat: Added the content for the `Generated` page.

--- app/badge/generated/page.tsx
+++ app/badge/generated/page.tsx
@@ -25,4 +25,13 @@ export default function GeneratedPage() {
 
         setParams({ text, svgName, backgroundColor, textColor, iconColor })
     }, [searchParams])
+
+    return (
+        <main>
+            <div style={{ backgroundColor: `#${params.backgroundColor}` }} className="w-[126.5px] h-[28px] flex items-center pl-2">
+                {params.svgName && <CustomSVG svgName={params.svgName} backgroundColor={params.backgroundColor} iconColor={params.iconColor} />}
+                {params.text && <label style={{ color: `#${params.textColor}` }} className="ml-3 uppercase font-bold text-xs tracking-widest">{params.text}</label>}
+            </div>
+        </main>
+    );
 }
